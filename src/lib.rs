607776bf1bbb7ab79d78962574ba37e3e6328c62
Bartek Kurpanik <bartekkur1@gmail.com>
2024-07-07 22:32:08 +0200

    Solved water bottles

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod add_two_numbers;
 pub mod palindrome_number;
 pub mod roman_to_integer;
 pub mod pass_the_pillow;
+pub mod water_bottles;
---
+++ src/water_bottles.rs
@@ -0,0 +1,33 @@
+struct Solution {}
+
+impl Solution {
+    pub fn num_water_bottles(num_bottles: i32, num_exchange: i32) -> i32 {
+        let mut sum = num_bottles;
+        let mut empty_bottles = num_bottles;
+
+        loop {
+            let exchanged_bottles = empty_bottles / num_exchange;
+            sum += exchanged_bottles;
+            let left_over = empty_bottles - exchanged_bottles * num_exchange;
+            empty_bottles = exchanged_bottles + left_over;
+
+            if empty_bottles < num_exchange {
+                break;
+            }
+        }
+
+        return sum;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(Solution::num_water_bottles(9, 3), 13);
+        assert_eq!(Solution::num_water_bottles(15, 4), 19);
+        assert_eq!(Solution::num_water_bottles(17, 3), 25);
+    }
+}
